90decfdb1c5a30627495a8bab9521fc870f94756
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-12 10:47:43 +0300

    update: comment list to filter comments by status

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,11 +1,27 @@
 const CommentList = ({ comments }) => {
-  return (
-    <ul>
-      {comments?.map((comment) => (
-        <li key={comment.id}>{comment.content}</li>
-      ))}
-    </ul>
-  );
+  const commentArr = comments?.map((comment) => {
+    let content;
+    switch (comment.status) {
+      case 'pending': {
+        content = 'This comment is pending moderation';
+        break;
+      }
+      case 'approved': {
+        content = comment.content;
+        break;
+      }
+      case 'rejected': {
+        content = 'This comment has been rejected';
+        break;
+      }
+      default: {
+        content = 'Unknown Error';
+      }
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{commentArr}</ul>;
 };
 
 export default CommentList;
