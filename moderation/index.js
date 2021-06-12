7a54d8e675cd9b2c18ab352a93f6457642a53cbd
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-12 10:11:52 +0300

    add: comment moderation service

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,23 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/events', (req, res) => {});
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('fuck') ? 'rejected' : 'approved';
+
+    axios.post('http://localhost:4005', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        content: data.content,
+        status,
+      },
+    });
+  }
+});
 
 app.listen(4003, () => {
   console.log('moderation => http://localhost:4003');
